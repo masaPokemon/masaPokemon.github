25d4f7e50494a05135f496725a37f74408bd5784
masaPokemon <masa.gongon2011@gmail.com>
2024-09-18 09:31:27 +0900

    Delete lib/src directory

--- lib/src/utils/configs.dart
+++
@@ -1,7 +0,0 @@
-const String appId = "476";
-const String authKey = "PDZjPBzAO8WPfCp";
-const String authSecret = "6247kjxXCLRaua6";
-
-// set custom API and Char server domains
-const String apiEndpoint = "https://api.connectycube.com";
-const String chatEndpoint = "chat.connectycube.com";
--- lib/src/utils/route_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Navigation {
-  static GlobalKey<NavigatorState> mainNavigation = GlobalKey();
-  static GlobalKey<NavigatorState> createDialogNavigation = GlobalKey();
-  static GlobalKey<NavigatorState> updateDialogNavigation = GlobalKey();
-  static GlobalKey<NavigatorState> verifyPhoneNavigation = GlobalKey();
-}
