62ca519e4399a171094b1352ccb5974213e34c5a
masaPokemon <masa.gongon2011@gmail.com>
2024-10-04 10:12:19 +0900

    Create player_model.dart

---
+++ lib/player_model.dart
@@ -0,0 +1,11 @@
+class Player {
+  final String name;
+  bool isAlive;
+  bool isWerewolf;
+
+  Player({
+    required this.name,
+    required this.isAlive,
+    required this.isWerewolf,
+  });
+}
