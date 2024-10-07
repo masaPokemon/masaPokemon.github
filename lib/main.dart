b16a3eb1e490a0ea6265cb0b3909b75411546bae
masaPokemon <masa.gongon2011@gmail.com>
2024-10-07 10:49:00 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'game_provider.dart';
-import 'studentScreen.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screen_capture/screen_capture.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,27 +9,65 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => GameProvider(),
-      child: MaterialApp(
-        home: GameScreen(),
+    return MaterialApp(
+      title: 'Classroom Monitoring',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: ClassroomScreen(),
     );
   }
 }
 
-class GameScreen extends StatelessWidget {
+class ClassroomScreen extends StatefulWidget {
+  @override
+  _ClassroomScreenState createState() => _ClassroomScreenState();
+}
+
+class _ClassroomScreenState extends State<ClassroomScreen> {
+  bool isRecording = false;
+
+  Future<void> _startScreenCapture() async {
+    // 権限のリクエスト
+    if (await Permission.mediaLibrary.request().isGranted) {
+      setState(() {
+        isRecording = true;
+      });
+      ScreenCapture.start();
+    } else {
+      // 権限が拒否された場合の処理
+      print('Screen capture permission denied');
+    }
+  }
+
+  Future<void> _stopScreenCapture() async {
+    ScreenCapture.stop();
+    setState(() {
+      isRecording = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Werewolf Game')),
-      body: TextField(
-        onChanged: (text) {
-          if (text == "p") {
-            
-          }
-        },
+      appBar: AppBar(
+        title: Text('Classroom Monitoring'),
       ),
-    )
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '生徒の画面を共有しています',
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: isRecording ? _stopScreenCapture : _startScreenCapture,
+              child: Text(isRecording ? '停止' : '開始'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
