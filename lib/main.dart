1c72eb19251f970f06815f50b6cd7dd39b8a7eb5
masaPokemon <masa.gongon2011@gmail.com>
2024-10-07 10:35:54 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'game_provider.dart';
-import 'player_model.dart';
+import 'studentScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,43 +24,13 @@ class GameScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Werewolf Game')),
-      body: Consumer<GameProvider>(
-        builder: (context, gameProvider, child) {
-          return Column(
-            children: [
-              Text(gameProvider.isNight ? "Night Time" : "Day Time", style: TextStyle(fontSize: 24)),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: gameProvider.players.length,
-                  itemBuilder: (context, index) {
-                    Player player = gameProvider.players[index];
-                    return ListTile(
-                      title: Text(player.name),
-                      subtitle: Text(player.isAlive ? "Alive" : "Dead"),
-                      trailing: player.isAlive && gameProvider.isNight
-                          ? ElevatedButton(
-                              onPressed: () {
-                                gameProvider.werewolfAttack(player.name);
-                              },
-                              child: Text("Attack"),
-                            )
-                          : null,
-                    );
-                  },
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  gameProvider.toggleDayNight();
-                },
-                child: Text(gameProvider.isNight ? "Switch to Day" : "Switch to Night"),
-              ),
-              if (gameProvider.gameOver)
-                Text("Game Over", style: TextStyle(fontSize: 32, color: Colors.red)),
-            ],
-          );
+      body: TextField(
+        onChanged: (text) {
+          if (text == "p") {
+            
+          }
         },
       ),
-    );
+    )
   }
 }
