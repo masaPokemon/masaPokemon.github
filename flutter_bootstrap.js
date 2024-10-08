17e305902b7e6f2439167c0139ca692ce90a3b37
masaPokemon <masaPokemon@users.noreply.github.com>
2024-10-08 02:57:55 +0000

    deploy: df6cd04579abb56320d33b602d6836bd296b0035

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"059e4e6d8ff6de39c29441c53e949bfb0bf179
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "733562527"
+    serviceWorkerVersion: "2280257428"
   }
 });
