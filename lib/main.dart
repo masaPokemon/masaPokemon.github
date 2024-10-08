78aa2158ea053034afeaf03ac5aeaf3edf7ecd42
masaPokemon <masa.gongon2011@gmail.com>
2024-10-08 11:40:56 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:screenshot/screenshot.dart';
 
 class ScreenCapture extends StatefulWidget {
