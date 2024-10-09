2f2e2cec25f116ee07e798dac99dbc25baa5c066
masaPokemon <masa.gongon2011@gmail.com>
2024-10-10 08:31:16 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'firebase_options.dart'; // Firebaseの設定ファイルをインポート
-import 'package:flutter/material.dart';
-import 'package:flutter_webrtc/flutter_webrtc.dart';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'home_screen.dart'; // この後で作成する画面のパス
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,81 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BroadcasterScreen(),
-    );
-  }
-}
-
-class BroadcasterScreen extends StatefulWidget {
-  @override
-  _BroadcasterScreenState createState() => _BroadcasterScreenState();
-}
-
-class _BroadcasterScreenState extends State<BroadcasterScreen> {
-  RTCPeerConnection? _peerConnection;
-  final DatabaseReference _dbRef = FirebaseDatabase.instance.reference().child('signaling');
-  final _localRenderer = RTCVideoRenderer();
-  MediaStream? _localStream;
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeRenderer();
-    _startBroadcast();
-  }
-
-  @override
-  void dispose() {
-    _localRenderer.dispose();
-    super.dispose();
-  }
-
-  Future<void> _initializeRenderer() async {
-    await _localRenderer.initialize();
-  }
-
-  Future<void> _startBroadcast() async {
-    // カメラや画面のストリームを取得
-    _localStream = await navigator.mediaDevices.getDisplayMedia({'video': true});
-
-    // WebRTCの接続設定
-    Map<String, dynamic> config = {
-      'iceServers': [
-        {'urls': 'stun:stun.l.google.com:19302'},
-      ]
-    };
-
-    _peerConnection = await createPeerConnection(config);
-
-    _localStream!.getTracks().forEach((track) {
-      _peerConnection!.addTrack(track, _localStream!);
-    });
-
-    // Firebaseにオファーを保存
-    var offer = await _peerConnection!.createOffer();
-    await _peerConnection!.setLocalDescription(offer);
-    _dbRef.child('offer').set(offer.toMap());
-
-    _peerConnection!.onIceCandidate = (RTCIceCandidate candidate) {
-      if (candidate != null) {
-        _dbRef.child('ice_candidates').push().set(candidate.toMap());
-      }
-    };
-
-    setState(() {
-      _localRenderer.srcObject = _localStream;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Broadcaster'),
+      title: 'Screen Sharing',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: RTCVideoView(_localRenderer),
-      ),
+      home: HomeScreen(), // ホーム画面に遷移
     );
   }
 }
