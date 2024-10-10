b4a82e9b096052b1521df74a7a1412a3296a8440
masaPokemon <masa.gongon2011@gmail.com>
2024-10-10 09:51:25 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'firebase_options.dart'; // Firebaseの設定ファイルをインポート
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'BroadcastScreen.dart';
-import 'ViewScreen.dart';
+import 'sign_in_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,49 +11,21 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter WebRTC',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  final TextEditingController _roomIdController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('WebRTC Screen Share'),
+      title: 'Screen Sharing App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _roomIdController,
-            decoration: InputDecoration(labelText: "Enter Room ID"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => BroadcastScreen(_roomIdController.text)),
-              );
-            },
-            child: Text("Start Broadcasting"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => ViewScreen(_roomIdController.text)),
-              );
-            },
-            child: Text("View Broadcast"),
-          ),
-        ],
-      ),
+      home: SignInScreen(), // サインイン画面を作成する
     );
   }
 }
