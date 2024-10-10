7b88667a4d24c9c58b156f11696d2831aba1d1d6
masaPokemon <masa.gongon2011@gmail.com>
2024-10-10 09:39:04 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,78 @@
 import 'firebase_options.dart'; // Firebaseの設定ファイルをインポート
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'home_screen.dart'; // この後で作成する画面のパス
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Broadcaster extends StatefulWidget {
+  @override
+  _BroadcasterState createState() => _BroadcasterState();
+}
+
+class _BroadcasterState extends State<Broadcaster> {
+  RTCPeerConnection? _peerConnection;
+  MediaStream? _localStream;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _createPeerConnection().then((pc) {
+      _peerConnection = pc;
+      _startLocalStream();
+    });
+  }
+
+  Future<void> _startLocalStream() async {
+    _localStream = await navigator.mediaDevices.getDisplayMedia({
+      'video': true,
+      'audio': false,
+    });
+    _peerConnection?.addStream(_localStream!);
+  }
+
+  Future<RTCPeerConnection> _createPeerConnection() async {
+    final config = {
+      'iceServers': [
+        {'urls': 'stun:stun.l.google.com:19302'}
+      ]
+    };
+    final pc = await createPeerConnection(config);
+    pc.onIceCandidate = (candidate) {
+      _firestore.collection('candidates').add(candidate.toMap());
+    };
+
+    final offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    _firestore.collection('offers').doc('broadcaster').set(offer.toMap());
+
+    return pc;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Broadcaster'),
+      ),
+      body: Center(
+        child: _localStream != null
+            ? RTCVideoView(_localStream!.getVideoTracks()[0].renderers[0])
+            : CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _localStream?.dispose();
+    _peerConnection?.close();
+    super.dispose();
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Screen Sharing',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(), // ホーム画面に遷移
-    );
-  }
+  runApp(Broadcaster());
 }
