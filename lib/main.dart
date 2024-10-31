d4fac31bea22c89db16f3ed450b79dd9263bab03
masaPokemon <masa.gongon2011@gmail.com>
2024-10-31 12:26:10 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'dart:async';
 import 'dart:math';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  options: DefaultFirebaseOptions.currentPlatform,
   runApp(MyApp());
 }
 
