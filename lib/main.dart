2bd235b2dad3c792de20cd8a9057fc8c134ce9a6
masaPokemon <masa.gongon2011@gmail.com>
2024-11-06 10:01:31 +0900

    Update main.darts

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'services/firebase_service.dart';
-import 'screens/game_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/result_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'game_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,); // Firebase初期化
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -17,17 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PvP人狼ゲーム',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/login': (context) => LoginScreen(),
-        '/game': (context) => GameScreen(),
-        '/result': (context) => ResultScreen(),
-      },
+      title: 'Werewolf Game',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: GameScreen(),
     );
   }
 }
