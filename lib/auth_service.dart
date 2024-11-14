95c70482405d2f9ee7e8fb4166d8085dff61c083
masaPokemon <masa.gongon2011@gmail.com>
2024-11-14 11:44:36 +0900

    Update auth_service.dart

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,24 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> signInWithGoogle() async {
-    final GoogleSignIn googleSignIn = GoogleSignIn();
-    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-    if (googleUser == null) {
-      return null; // ユーザーがサインインしなかった場合
+  // メールでサインイン
+  Future<User?> signInWithEmailPassword(String email, String password) async {
+    try {
+      final UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print("Error signing in: $e");
+      return null;
     }
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    final OAuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    final UserCredential userCredential = await _auth.signInWithCredential(credential);
-    return userCredential.user;
   }
 
+  // メールでサインアップ
+  Future<User?> signUpWithEmailPassword(String email, String password) async {
+    try {
+      final UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print("Error signing up: $e");
+      return null;
+    }
+  }
+
+  // サインアウト
   Future<void> signOut() async {
     await _auth.signOut();
   }
