1d5d792771e49bfc5a7ffc50b259b7b63c870347
masaPokemon <masa.gongon2011@gmail.com>
2024-11-20 09:01:18 +0900

    Update ranking_screen.dart

--- lib/screens/ranking_screen.dart
+++ lib/screens/ranking_screen.dart
@@ -1,66 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
 
-class RankingScreen extends StatefulWidget {
-  @override
-  _RankingScreenState createState() => _RankingScreenState();
-}
-
-class _RankingScreenState extends State<RankingScreen> {
-  final DatabaseReference _db = FirebaseDatabase.instance.ref().child('scores');
-  late Stream<List<Player>> _rankingStream;
-
-  @override
-  void initState() {
-    super.initState();
-    _rankingStream = _db.orderByChild('score').onValue.map((event) {
-      var players = <Player>[];
-      var data = event.snapshot.value as Map<dynamic, dynamic>;
-      data.forEach((key, value) {
-        players.add(Player.fromMap(value));
-      });
-      return players;
-    });
-  }
-
+class RankingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ランキング')),
-      body: StreamBuilder<List<Player>>(
-        stream: _rankingStream,
+      appBar: AppBar(title: Text('Ranking')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .orderBy('wins', descending: true)
+            .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
             return Center(child: CircularProgressIndicator());
           }
 
-          final players = snapshot.data!;
+          var users = snapshot.data!.docs;
           return ListView.builder(
-            itemCount: players.length,
+            itemCount: users.length,
             itemBuilder: (context, index) {
-              final player = players[index];
+              var user = users[index];
               return ListTile(
-                title: Text(player.name),
-                trailing: Text(player.score.toString()),
+                title: Text(user['name']),
+                subtitle: Text('Wins: ${user['wins']} Losses: ${user['losses']}'),
               );
             },
           );
         },
       ),
-    );
-  }
-}
-
-class Player {
-  final String name;
-  final int score;
-
-  Player({required this.name, required this.score});
-
-  factory Player.fromMap(Map<dynamic, dynamic> map) {
-    return Player(
-      name: map['name'],
-      score: map['score'],
     );
   }
 }
