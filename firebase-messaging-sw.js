d09eb73d3bfe9cafa2ec5add2a68cd40f451f814
masaPokemon <masaPokemon@users.noreply.github.com>
2024-12-10 01:14:32 +0000

    deploy: 5193166dc425678998f80dc673c5c4883bae6750

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,26 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+// Please see this file for the latest firebase-js-sdk version:
+// https://github.com/firebase/flutterfire/blob/master/packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",
+  apiKey: "AIzaSyD_ddEwE9Cy9rvh7Rdf5ddVBAY8aEKj9cM",
+  authDomain: "seat-change-optimization.firebaseapp.com",
+  databaseURL: "https://seat-change-optimization-default-rtdb.firebaseio.com",
+  storageBucket: "seat-change-optimization.firebasestorage.app",
+  projectId: "seat-change-optimization",
+  appId: "1:416064751674:web:a2808bdfffe75a012ccd03",
+  messagingSenderId: "416064751674",
+  measurementId: "G-Q6TSN9L1TN"
   projectId: "...",
   storageBucket: "...",
   messagingSenderId: "...",
   appId: "...",
-  measurementId: "..."
 });
 
 const messaging = firebase.messaging();
 
+// Optional:
 messaging.onBackgroundMessage((message) => {
   console.log("onBackgroundMessage", message);
-    if (Notification.permission === "granted") {
-      self.registration.showNotification('Connectycube Flutter Chat sample', {body: message.data.message})
-    }
-
-    else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(function (permission) {
-        if (permission === "granted") {
-          self.registration.showNotification('Connectycube Flutter Chat sample', {body: message.data.message})
-        }
-      });
-    }
 });
