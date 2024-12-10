98a18351a005656023f2fd2a17c6abd69d68b638
masaPokemon <masa.gongon2011@gmail.com>
2024-12-10 10:03:09 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,152 +1,60 @@
-/// Flutter関係のインポート
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'dart:async';
-
-/// Firebase関係のインポート
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-/// プラットフォームの確認
-final isAndroid =
-    defaultTargetPlatform == TargetPlatform.android ? true : false;
-final isIOS = defaultTargetPlatform == TargetPlatform.iOS ? true : false;
-
-/// FCMバックグランドメッセージの設定
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  print('Handling a background message: ${message.messageId}');
-}
-
-/// メイン
 void main() async {
-  /// クラッシュハンドラ
-  runZonedGuarded<Future<void>>(() async {
-    /// Firebaseの初期化
-    WidgetsFlutterBinding.ensureInitialized();
-
-    /// FCMのバックグランドメッセージを表示
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-    await Firebase.initializeApp(
-      name: isAndroid || isIOS ? 'counterFirebase' : null,
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-
-    /// クラッシュハンドラ(Flutterフレームワーク内でスローされたすべてのエラー)
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-
-    /// runApp w/ Riverpod
-    runApp(const ProviderScope(child: MyApp()));
-  },
-
-      /// クラッシュハンドラ(Flutterフレームワーク内でキャッチされないエラー)
-      (error, stack) =>
-          FirebaseCrashlytics.instance.recordError(error, stack, fatal: true));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-/// MaterialAppの設定
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter Firebase',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase Messaging',
+      home: MyHomePage(),
     );
   }
 }
 
-/// ホーム画面
-class MyHomePage extends ConsumerStatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+class MyHomePage extends StatefulWidget {
   @override
-  MyHomePageState createState() => MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class MyHomePageState extends ConsumerState<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
   @override
   void initState() {
     super.initState();
+    
+    // フォアグラウンド時に通知を受け取るリスナー
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Received a message: ${message.notification?.title}');
+    });
 
-    /// FCMのパーミッション設定
-    FirebaseMessagingService().setting();
+    // バックグラウンドで通知を受け取るリスナー
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Message clicked! ${message.notification?.title}');
+    });
 
-    /// FCMのトークン表示(テスト用)
-    FirebaseMessagingService().fcmGetToken();
+    // FCMトークンを取得
+    _firebaseMessaging.getToken().then((token) {
+      print("FCM Token: $token");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            // デフォルト表示
-            Text('Default'),
-            // 太さを指定
-            Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
-            // スタイルを指定
-            Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
-            // サイズを指定
-            Text('fontSize = 36', style: TextStyle(fontSize: 36)),
-            // 色を指定
-            Text('Red', style: TextStyle(color: Colors.red)),
-            Container(
-              width: double.infinity,
-              color: Colors.grey,
-              // 表示位置を指定
-              child: Text('TextAlign.right', textAlign: TextAlign.right),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Messaging'),
+      ),
+      body: Center(
+        child: Text('Push notifications will appear here'),
       ),
     );
   }
 }
-
-/// Firebase Cloud Messageの設定
-class FirebaseMessagingService {
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-  /// webとiOS向け設定
-  void setting() async {
-    NotificationSettings settings = await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-
-    print('User granted permission: ${settings.authorizationStatus}');
-  }
-
-  void fcmGetToken() async {
-    /// モバイル向け
-    if (isAndroid || isIOS) {
-      final fcmToken = await messaging.getToken();
-      print(fcmToken);
-    }
-    // web向け
-    else {
-      final fcmToken = await messaging.getToken(
-          vapidKey: FirebaseOptionMessaging().webPushKeyPair);
-      print('web : $fcmToken');
-    }
-  }
-}
-    
